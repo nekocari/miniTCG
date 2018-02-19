bb82f122b69d79903f5b92908ab60ae5b6acc63f
Cari <Cari@Cari_Asus>
2018-02-19 14:02:37 +0100

    initial commit

---
+++ inc/constants.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(E_ALL);
+if($_SERVER['SERVER_NAME'] == 'localhost'){
+    // ------------------------------------------------------------------------------ L O C A L
+    define('MYSQL_HOST',     'localhost'); 			// Host Name, meist passt localhost
+    define('MYSQL_USER',     'root'); 				// Benutzername
+    define('MYSQL_PASS',     '');		 			// Benutzerpasswort
+    define('MYSQL_DATABASE', 'witchwood');	 		// Datenbank Name
+    
+    define('PROJECT_FOLDER','ww_mvc');
+    define('PATH', dirname(__FILE__).'/../'); 				// relativer Pfad
+    define('BASE_URI', 'http://localhost/'.PROJECT_FOLDER.'/'); 			// absoluter Pfad
+    define('SERVER_URL', $_SERVER['SERVER_NAME']);
+}
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'inc/constants.php';
+require_once 'inc/dbconnect.php';
+
+if(!$db = Db::getInstance()){ die('DB not connected'); }
+
+
+
+?>
