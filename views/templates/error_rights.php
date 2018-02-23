1e67a1e59a872c768ae59500f958774e1f794c71
Cari <chibi_cari@web.de>
2018-02-23 12:28:32 +0100

    added memberlist and member edit fuction + htaccess files to deny access of internal folders

--- views/templates/error_rights.php
+++ views/templates/error_rights.php
@@ -1,5 +1,5 @@
 <h1>Keine Berechtigung</h1>
 
-<p>Du hast nicht die nötigen Rechte um diese Seite aufzurufen.</p>
+<p>Du hast nicht die nötigen Rechte um diese Seite anzuzeigen.</p>
 
 <p><button class="btn btn-primary" onclick="history.go(-1);">zurück</button></p>
