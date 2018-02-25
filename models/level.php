177458a3bf821e92e1373cb1782ad183fecd6b5a
Cari <chibi_cari@web.de>
2018-02-25 01:54:03 +0100

    added level

---
+++ models/level.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * 
+ * Represents a Level
+ * 
+ * @author Cari
+ *
+ */
+class Level {
+    
+    private $id;
+    private $level;
+    private $name;
+    private $cards;
+    private static $naming_pattern = '/[A-Za-z0-9äÄöÖüÜß _\-]+/';
+    private $db;
+    
+    
+    public function __construct($id, $level, $name, $cards) {
+        $this->id = $id;
+        $this->level = $level;
+        $this->name = $name;
+        $this->cards = $cards;
+        $this->db = Db::getInstance();
+    }
+    
+    /**
+     * get data of all levels from database
+     * 
+     * @param Db $db_conn PDO connection object
+     * 
+     * @return boolean|Level[]
+     */
+    public static function getAll() {
+        $level = array();
+        $db_conn = Db::getInstance();
+    
+        $req = $db_conn->query("SELECT * FROM level ORDER BY level ASC");
+        if($req->execute()){
+            foreach($req->fetchAll() as $data) {
+                $level[] = new Level($data['id'], $data['level'], $data['name'], $data['cards']);
+            }
+        }else{
+            return false;
+        }
+        
+        return $level;
+    }
+    
+    /**
+     * get level data from database using id number
+     * 
+     * @param int $id Id number of level in database
+     * @param Db $db_conn PDO connection object
+     * 
+     * @return boolean|Level 
+     */
+    public static function getById($id) {
+        $level = false;
+        $db_conn = Db::getInstance();
+        
+        $req = $db_conn->prepare('SELECT * FROM level WHERE id = :id');
+        if($req->execute(array(':id' => $id))) {
+            if($req->rowCount()) {
+                $data = $req->fetch();
+                $level = new Level($data['id'], $data['name'], $data['cards']);
+            }
+        }else{
+            return false;
+        }
+        
+        return $level;
+    }
+    
+    public static function add($name, $cards) {
+        $db_conn = Db::getInstance();
+        try {
+            if(preg_match(self::$naming_pattern, $name) AND intval($cards) >= 0){
+                $req = $db_conn->prepare('INSERT INTO level (name, cards) VALUES (:name, :cards)');
+                $req->execute(array(':name'=>$name, 'cards'=>$cards));
+                return true;
+            }else{
+                throw new Exception('Name enthält mindestens ein ungültiges Zeichen.');
+            }
+        }
+        catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+    
+    public function update($name, $cards) {
+        try {
+            if(preg_match(self::$naming_pattern, $name) AND intval($cards) >= 0){
+                $req = $this->db->prepare('UPDATE level SET name = :name, cards = :cards WHERE id = :id');
+                $req->execute(array(':name'=>$name, ':cards'=>$cards, ':id'=>$this->id));
+                $this->name = $name;
+                $this->cards = $cards;
+                return true;
+            }else{
+                throw new Exception('Name enthält mindestens ein ungültiges Zeichen.');
+            }
+        }
+        catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+    
+    /*
+     * Getter
+     */
+    public function getCards() {
+        return $this->cards;
+    }
+    public function getLevel() {
+        return $this->level;
+    }
+    public function getName() {
+        return $this->name;
+    }
+    public function getId() {
+        return $this->id;
+    }
+    
+}
---
+++ views/admin/level/edit.php
@@ -0,0 +1,36 @@
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="admin/">Verwaltung</a></li>
+    <li class="breadcrumb-item"><a href="admin/level.php">Level</a></li>
+    <li class="breadcrumb-item active" aria-current="page">bearbeiten</li>
+  </ol>
+</nav>
+
+<h1>Level bearbeiten</h1>
+
+<form method="post" action="">
+    <table class="table table-striped">
+    	<tr>
+    		<td>Level ID</td>
+    		<td><input class="form-control" type="text" name="id" value="<?php echo $level->getId(); ?>" disabled></td>
+    	</tr>
+    	<tr>
+    		<td>Level Stufe:</td>
+    		<td><input class="form-control" type="number" name="level" value="<?php echo $level->getLevel(); ?>" required></td>
+    	</tr>
+    	<tr>
+    		<td>Level Name:</td>
+    		<td><input class="form-control" type="text" name="name" value="<?php echo $level->getName(); ?>" required></td>
+    	</tr>
+    	<tr>
+    		<td>Benötigte Karten Anzahl:</td>
+    		<td><input class="form-control" type="number" name="cards" value="<?php echo $level->getCards(); ?>" required></td>
+    	</tr>
+    </table>
+
+	<p class="text-center">
+		<a class="btn btn-dark" href="admin/level.php">zurück zur Liste</a> 
+		&bull; <input class="btn btn-primary" type="submit" name="editLevel" value="speichern">
+		<input type="hidden" name="id" value="<?php echo $level->getId(); ?>">
+	</p>
+</form>
