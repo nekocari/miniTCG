c20bd1bff1c436f8a34aacf8edc5bdbbfc2c8722
nekocari <neko.cari@gmail.com>
2024-10-10 22:14:14 +0200

    fixed issue - login button not visible on small screens

--- app/views/en/templates/layout/2/header.php
+++ app/views/en/templates/layout/2/header.php
@@ -8,9 +8,9 @@
 		<base href="<?php echo BASE_URI; ?>">
 		<title><?php if(empty($this->getTitle())){ echo Setting::getByName('app_name')->getValue(); }else{ echo $this->getTitle(); } ?></title>
 		
-    	<!-- default bootstrap 5 -->
-		<link href="public/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">		
-		<script src="public/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+		<!-- default bootstrap 5 -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     	<!-- Custom CSS -->
 		<link rel="stylesheet" type="text/css" href="public/css/style.css">
 		
@@ -84,7 +84,7 @@
 					</button>
 					
 					
-					<!-- NOT LOGGED IN -->
+					<!-- LOGGED IN -->
 					<?php if($this->login->isLoggedIn()) { ?>  
 						<div class="navbar-expand" id="navbar2">
 								
@@ -172,8 +172,8 @@
 					
 					<!-- NOT LOGGED IN -->
 					<?php if(!$this->login->isLoggedIn()) { ?>  
-						<div class="collapse navbar-collapse justify-content-end" id="navbar2">
-							<ul class="navbar-nav">
+						<div class="justify-content-end" id="navbar2">
+							<ul class="nav">
 								<li class="nav-item">
 									<a class="btn btn-outline-dark" href="<?php echo Routes::getUri('signin');?>">Login</a>
 								</li>
