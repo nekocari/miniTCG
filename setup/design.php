ffaa91305217d9d679cf83b770cd20a61888af10
nekocari <neko.cari@gmail.com>
2024-10-19 21:33:00 +0200

    removed unused css from setup design file

--- setup/design.php
+++ setup/design.php
@@ -10,7 +10,6 @@
 		<!-- Bootstrap CSS -->
     	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     	<!-- Custom CSS -->
-		<link rel="stylesheet" type="text/css" href="public/css/style.css">
 		
 	</head>
 	<body>
